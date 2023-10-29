a5c235721014af5bf5e50dca25ff9c68791d91a6
selfkilla666 <selfkilla666@yahoo.com>
2023-10-29 04:31:52 +0200

    Blank application
    Signed-off-by: selfkilla666 <selfkilla666@yahoo.com>

---
+++ crasher/application.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import sys
+from typing import NoReturn
+from PySide6.QtWidgets import QApplication
+
+from loguru import logger
+
+from crasher.widgets.window import QCrasherWindow
+from crasher.utils.path import get_user_local_directory
+
+APPLICATION_TITLE: str = "Crasher"
+APPLICATION_VERSION: str = "1.0.0b"
+APPLICATION_ORG_NAME: str = "Witch Software"
+APPLICATION_ORG_DOMAIN: str = "witch-software.com"
+
+
+def run_application() -> NoReturn:
+
+    # Setup logger
+    logger.add(str(get_user_local_directory()) + "logs/log_{time}.log",
+               format="{time:HH:mm:ss.SS} | {file} | {level} | {message}", colorize=True)
+
+    logger.info("Application starts")
+
+    # Setup Qt application
+    application: QApplication = QApplication(sys.argv)
+
+    # Set application metadata
+    application.setApplicationName(APPLICATION_TITLE)
+    application.setApplicationVersion(APPLICATION_VERSION)
+    application.setOrganizationName(APPLICATION_ORG_NAME)
+    application.setOrganizationDomain(APPLICATION_ORG_DOMAIN)
+
+    logger.info("Initialize application window...")
+
+    # Initialize application window
+    window: QCrasherWindow = QCrasherWindow(
+        application=application,
+        logger=logger
+    )
+
+    # Start application
+    window.show()
+    application.exec()
+
+    logger.success("Application was successfully closed.")
+
+
+if __name__ == "__main__":
+    run_application()
---
+++ crasher/widgets/window.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from loguru import logger
+
+
+__all__ = ["QCrasherWindow"]
+
+
+class QCrasherWindow(QMainWindow):
+    """ Class of application window for Crasher """
+
+    # Todo: Add menu- & statusbar
+    # Todo: Make hello central widget
+    # Todo: Set icon and geometry for window at initialize_ui()
+    # Todo: Add to statusbar handler of logger messages
+    # Todo: Add onClose event handler
+    # Todo: Add commentaries at code
+
+    def __init__(self, *, application: QApplication, logger: logger) -> None:
+        super().__init__()
+
+        self.application: QApplication = application
+        self.logger: logger = logger
+
+        self.initialize_ui()
+        self.logger.success("Window initialized!")
+
+    def initialize_ui(self) -> None:
+
+        self.logger.info("Initializing UI...")
+
+        self.setWindowTitle(self.application.applicationName())

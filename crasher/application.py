bfac57d23c420d745cd9e82b4b362e9a416c5290
selfkilla666 <selfkilla666@yahoo.com>
2024-09-27 19:13:45 +0300

    Update application CLI
    Signed-off-by: selfkilla666 <selfkilla666@yahoo.com>

--- crasher/application.py
+++ crasher/application.py
@@ -1,31 +1,82 @@
-# Code by @selfkilla666
-# https://github.com/witch-software/crasher
-# MIT License
 
 from __future__ import annotations
-
-from crasher.core.widgets.application import QCrasherApplication
 
 import sys
 import argparse
 
+from crasher.core.widgets.application import QCrasherApplication
+
+
+DEFAULT_WINDOWLESS: bool = False
+DEFAULT_DEBUG: bool = False
+
+
+DESCRIPTION: str = """
+Crasher application with launch arguments. \n\n
+
+Crasher is an open-source application designed for creating glitch art using Python.
+It provides tools for generating unique visual distortions in images, offering a 
+creative platform for artists and developers interested in glitch aesthetics.
+"""
+
 
 def get_run_arguments() -> argparse.Namespace:
-    """ Get application startup arguments """
+    """
+    Parse command line arguments for the application.
 
-    argument_parser: argparse.ArgumentParser = argparse.ArgumentParser(
+    :return: argparse.Namespace: The parsed command line arguments.
+    """
+
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog="crasher",
-        description="Crasher application with launch arguments"
+        description=DESCRIPTION
     )
 
-    argument_parser.add_argument("--windowless", action="store_true", help="run in windowless mode", default=False)
-    argument_parser.add_argument("--debug", action="store_true", help="run application in debug mode", default=False)
+    # General Options
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Run application in debug mode",
+        default=DEFAULT_DEBUG
+    )
+    parser.add_argument(
+        "--windowless",
+        action="store_true",
+        help="Run in windowless mode",
+        default=DEFAULT_WINDOWLESS
+    )
 
-    return argument_parser.parse_args()
+    # Project Options
+    parser.add_argument(
+        "-r", "--render",
+        action="store_true",
+        help="Render the current project",
+        default=False
+    )
+    parser.add_argument(
+        "-o", "--open",
+        dest="filename",
+        type=argparse.FileType("r", encoding="UTF-8"),
+        help="Open a specific project file",
+        metavar="PATH",
+        default=None
+    )
+    parser.add_argument(
+        "-f", "--file",
+        dest="filename",
+        type=argparse.FileType("r", encoding="UTF-8"),
+        help="Output rendered image to specific file",
+        metavar="PATH",
+        default=None
+    )
+
+    return parser.parse_args()
 
 
 def run_application() -> None:
+    """ Set up and run the application. """
 
+    # Get run arguments for application
     args = get_run_arguments()
 
     # Setup application

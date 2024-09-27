5c5b9eb77e8a50e1ba93c8bb6410b19b2e06fac7
selfkilla666 <selfkilla666@yahoo.com>
2024-09-28 00:20:18 +0300

    Return __future__.annotations for working app
    Signed-off-by: selfkilla666 <selfkilla666@yahoo.com>

--- crasher/core/widgets/menubar.py
+++ crasher/core/widgets/menubar.py
@@ -1,4 +1,6 @@
 
+from __future__ import annotations
+
 from typing import Optional, TYPE_CHECKING
 
 from PySide6.QtWidgets import QMenuBar, QMenu
--- crasher/core/widgets/window.py
+++ crasher/core/widgets/window.py
@@ -1,4 +1,6 @@
 
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from PySide6.QtCore import Slot as qtSlot
